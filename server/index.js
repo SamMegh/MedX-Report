a0bf9992b4e6b6f9b4e79b52fac96c553c6e3aab
Ankit <sam.megh0305@gmail.com>
2025-08-30 17:43:48 +0530

    data base is connected and auth routes are ready to deploy

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import authRouter from "./src/routes/auth.route.js"
+import { connectToDB } from './src/lib/monos.db.connection.js';
 dotenv.config();
 const app = express();
 const port = process.env.Port;
@@ -18,4 +19,5 @@ app.use("/",authRouter);
 
 app.listen(port,()=>{
     console.log("server started at : "+port);
+    connectToDB();
 })
