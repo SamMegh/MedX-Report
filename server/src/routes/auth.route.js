dc8f2ee09d1583cf87965cfd9a187470fd4c574d
Ankit <sam.megh0305@gmail.com>
2025-09-02 18:56:47 +0530

    protection added to all the routes

--- server/src/routes/auth.route.js
+++ server/src/routes/auth.route.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import { checkToken, login, logout, signup, updateProfile } from '../controller/auth.controller.js';
+import Protection from '../mid-layer/protection.js';
 
 const router = express.Router();
-router.post("/login",login)
-router.post("/signup",signup)
-router.get("/check",checkToken)
-router.get("/logout",logout)
-router.post("/profile",updateProfile)
+router.post("/login", login)
+router.post("/signup", signup)
+router.get("/check", Protection, checkToken)
+router.get("/logout", Protection, logout)
+router.post("/profile", Protection, updateProfile)
 
 export default router
