f457f3ed5ef4ae41bf698100db69b8145db40edc
Ankit <sam.megh0305@gmail.com>
2025-09-02 15:20:53 +0530

    use auth store is done

--- client/src/store/useAuthStore.js
+++ client/src/store/useAuthStore.js
@@ -1,15 +1,44 @@
 import { create } from 'zustand'
 import { Instance } from '../lib/axios.config';
 
-export const useAuthStore = create((set)=>({
-    isAuthUser:null,
-login:async(data)=>{
-    try {
-        const res= await Instance.post("/login",data);
-        console.log(res.data)
-        set({isAuthUser:res.data});
-    } catch (error) {
-        console.log("unable to login", error);
+export const useAuthStore = create((set) => ({
+    isAuthUser: null,
+    login: async (data) => {
+        try {
+            const res = await Instance.post("/login", data);
+            console.log(res.data)
+            set({ isAuthUser: res.data });
+        } catch (error) {
+            console.log("unable to login", error);
+        }
+    },
+
+    signup: async (data) => {
+        try {
+            const res = await Instance.post("/signup", data);
+            console.log(res.data)
+            set({ isAuthUser: res.data });
+        } catch (error) {
+            console.log("unable to login", error);
+        }
+    },
+
+    check: async () => {
+        try {
+            const res = await Instance.post("/check");
+            console.log(res.data)
+            set({ isAuthUser: res.data });
+        } catch (error) {
+            console.log("unable to login", error);
+        }
+    },
+
+    logout: async () => {
+        try {
+            await Instance.post("/logout");
+            set({ isAuthUser: null });
+        } catch (error) {
+            console.log("unable to login", error);
+        }
     }
-}
 }))
